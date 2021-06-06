b4f8a53a84f81d08324ca378e5dbcf67c7606c6f
knada <nanaayimda@gmail.com>
2021-06-06 05:45:55 +0000

    Created navigation for app

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,50 +1,44 @@
-/**
- * If you are not familiar with React Navigation, check out the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
-import {
-    NavigationContainer,
-    DefaultTheme,
-    DarkTheme,
-} from '@react-navigation/native'
+import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import * as React from 'react'
-import { ColorSchemeName } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import LoginScreen from '../screens/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen'
+import HomeScreen from '../screens/HomeScreen'
 
-import NotFoundScreen from '../screens/NotFoundScreen'
-import { RootStackParamList } from '../types'
-import BottomTabNavigator from './BottomTabNavigator'
-import LinkingConfiguration from './LinkingConfiguration'
+const Stack = createStackNavigator()
 
-export default function Navigation({
-    colorScheme,
-}: {
-    colorScheme: ColorSchemeName
-}) {
+const AppStack = () => {
     return (
-        <NavigationContainer
-            linking={LinkingConfiguration}
-            theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-            <RootNavigator />
-        </NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Register"
+                component={RegisterScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    title: 'Climb Time',
+                    headerTitleStyle: {
+                        fontFamily: 'MontserratBlack',
+                        color: '#F5C669',
+                    },
+                }}
+            />
+        </Stack.Navigator>
     )
 }
 
-// A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
-const Stack = createStackNavigator<RootStackParamList>()
-
-function RootNavigator() {
+export default function Navigation() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-            <Stack.Screen
-                name="NotFound"
-                component={NotFoundScreen}
-                options={{ title: 'Oops!' }}
-            />
-        </Stack.Navigator>
+        <NavigationContainer>
+            <AppStack />
+        </NavigationContainer>
     )
 }
